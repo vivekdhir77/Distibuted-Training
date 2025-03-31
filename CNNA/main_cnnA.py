74886bcfbb7364ba340b5ec594e67607248b50b3
vivekdhir77 <vivekdhir77@gmail.com>
2025-03-31 18:42:35 -0400

    changed cnnA architecture

--- CNNA/main_cnnA.py
+++ CNNA/main_cnnA.py
@@ -36,28 +36,24 @@ classes = ('plane', 'car', 'bird', 'cat',
 
 
 class ConvNet(nn.Module):
-    '''Basic CNN architecture'''
+    '''Smaller CNN architecture'''
     def __init__(self):
         super(ConvNet, self).__init__()
-        #define layers
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool=nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        
+        # Define layers
+        self.conv1 = nn.Conv2d(3, 4, 3)  # Reduced number of filters and kernel size
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(4, 8, 3)  # Reduced number of filters
+
         # FC layers
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 36)
-        self.fc3 = nn.Linear(36, 72)
-        self.fc4 = nn.Linear(72, 10)    # 10 classes
+        self.fc1 = nn.Linear(8 * 6 * 6, 32)  # Reduced number of neurons
+        self.fc2 = nn.Linear(32, 10)  # Directly map to 10 classes
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))  # first conv+pool
-        x = self.pool(F.relu(self.conv2(x)))  # second conv+pool
-        x = x.view(-1, 16 * 5 * 5)            
-        x = F.relu(self.fc1(x))           
-        x = F.relu(self.fc2(x))        
-        x = F.relu(self.fc3(x))                       
-        x = self.fc4(x) 
+        x = self.pool(F.relu(self.conv1(x)))  # First conv+pool
+        x = self.pool(F.relu(self.conv2(x)))  # Second conv+pool
+        x = x.view(-1, 8 * 6 * 6)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
 def load_data(batch_size):
